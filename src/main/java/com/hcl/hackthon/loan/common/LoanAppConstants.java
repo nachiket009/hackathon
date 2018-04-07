bcfb6dbb614d58be63a16be5c1bbd8e1d959b877
nachiket <nphalnikar@gmail.com>
2018-04-07 16:47:46 +0530

    df 1

--- src/main/java/com/hcl/hackthon/loan/common/LoanAppConstants.java
+++ src/main/java/com/hcl/hackthon/loan/common/LoanAppConstants.java
@@ -7,6 +7,6 @@ public class LoanAppConstants {
 	}
 	public static final float RATE_OF_INTEREST = 9.0f;
 	public static final String PENDING = "PENDING";
-	public static final String APPROVED = "APPROVED";
+	public static final String APPROVED = "APROVED";
 	
 }
